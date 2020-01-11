93799b37ac7377c088a6deace4211c8da6eac552
WenZheng Li <me@wenzheng.li>
2020-01-12 00:00:18 +0900

    prepend STRUCT to macro names

--- mxgen.h
+++ mxgen.h
@@ -3,56 +3,56 @@
 #include <stdio.h>
 #include <string.h>
 
-#define GEN_STRUCT(STRUCT)         \
-    GEN_STRUCT_DEFINITION(STRUCT); \
-    GEN_COMPARE(STRUCT);           \
-    GEN_SERIALIZE(STRUCT);         \
-    GEN_DESERIALIZE(STRUCT);       \
-    GEN_TO_JSON(STRUCT);
+#define GEN_STRUCT(STRUCT)          \
+    GEN_STRUCT_DEFINITION(STRUCT);  \
+    GEN_STRUCT_COMPARE(STRUCT);     \
+    GEN_STRUCT_SERIALIZE(STRUCT);   \
+    GEN_STRUCT_DESERIALIZE(STRUCT); \
+    GEN_STRUCT_TO_JSON(STRUCT);
 
 #define GEN_STRUCT_FIELD(TYPE, NAME, ARRAY) TYPE NAME ARRAY;
 #define GEN_STRUCT_DEFINITION(STRUCT) typedef struct { STRUCT_##STRUCT(GEN_STRUCT_FIELD) } STRUCT
 
-#define GEN_COMPARE_FIELD(TYPE, NAME, ARRAY)                                  \
+#define GEN_STRUCT_COMPARE_FIELD(TYPE, NAME, ARRAY)                           \
     for (size_t i = 0; !result && i < sizeof(TYPE ARRAY) / sizeof(TYPE); ++i) \
         result |= TYPE##_compare((TYPE*) &a->NAME + i, (TYPE*) &b->NAME + i);
 
-#define GEN_SERIALIZE_FIELD(TYPE, NAME, ARRAY)                     \
+#define GEN_STRUCT_SERIALIZE_FIELD(TYPE, NAME, ARRAY)              \
     for (size_t i = 0; i < sizeof(TYPE ARRAY) / sizeof(TYPE); ++i) \
         len += TYPE##_serialize((TYPE*) &struc->NAME + i, buf + len);
 
-#define GEN_DESERIALIZE_FIELD(TYPE, NAME, ARRAY)                   \
+#define GEN_STRUCT_DESERIALIZE_FIELD(TYPE, NAME, ARRAY)            \
     for (size_t i = 0; i < sizeof(TYPE ARRAY) / sizeof(TYPE); ++i) \
         len += TYPE##_deserialize((TYPE*) &struc->NAME + i, buf + len);
 
-#define GEN_COMPARE(STRUCT)                                                \
+#define GEN_STRUCT_COMPARE(STRUCT)                                         \
     static inline int STRUCT##_compare(const STRUCT* a, const STRUCT* b) { \
         int result = 0;                                                    \
-        STRUCT_##STRUCT(GEN_COMPARE_FIELD);                                \
+        STRUCT_##STRUCT(GEN_STRUCT_COMPARE_FIELD);                         \
         return result;                                                     \
     }
 
-#define GEN_SERIALIZE(STRUCT)                                                 \
+#define GEN_STRUCT_SERIALIZE(STRUCT)                                          \
     static inline int STRUCT##_serialize(const STRUCT* struc, uint8_t* buf) { \
         int len = 0;                                                          \
-        STRUCT_##STRUCT(GEN_SERIALIZE_FIELD);                                 \
+        STRUCT_##STRUCT(GEN_STRUCT_SERIALIZE_FIELD);                          \
         return len;                                                           \
     }
 
-#define GEN_DESERIALIZE(STRUCT)                                                 \
+#define GEN_STRUCT_DESERIALIZE(STRUCT)                                          \
     static inline int STRUCT##_deserialize(STRUCT* struc, const uint8_t* buf) { \
         int len = 0;                                                            \
-        STRUCT_##STRUCT(GEN_DESERIALIZE_FIELD);                                 \
+        STRUCT_##STRUCT(GEN_STRUCT_DESERIALIZE_FIELD);                          \
         return len;                                                             \
     }
 
-#define GEN_JSON_FIELD_STRING(NAME)          \
+#define GEN_STRUCT_JSON_FIELD_STRING(NAME)   \
     buf[len++] = '"';                        \
     strcpy(buf + len, (char*) &struc->NAME); \
     len += strlen((char*) &struc->NAME);     \
     buf[len++] = '"';
 
-#define GEN_JSON_FIELD_LIST(TYPE, NAME, ARRAY)                       \
+#define GEN_STRUCT_JSON_FIELD_LIST(TYPE, NAME, ARRAY)                \
     buf[len++] = '[';                                                \
     for (size_t i = 0; i < sizeof(TYPE ARRAY) / sizeof(TYPE); ++i) { \
         len += TYPE##_to_json((TYPE*) &struc->NAME + i, buf + len);  \
@@ -61,29 +61,29 @@
     }                                                                \
     buf[--len - 1] = ']';
 
-#define GEN_JSON_FIELD(TYPE, NAME, ARRAY)                       \
+#define GEN_STRUCT_JSON_FIELD(TYPE, NAME, ARRAY)                \
     len += sprintf(buf + len, "\"%s\":", #NAME);                \
     if (sizeof(TYPE ARRAY) == sizeof(TYPE)) {                   \
         len += TYPE##_to_json((TYPE*) &struc->NAME, buf + len); \
     } else if (strcmp(#TYPE, "char")) {                         \
-        GEN_JSON_FIELD_LIST(TYPE, NAME, ARRAY);                 \
+        GEN_STRUCT_JSON_FIELD_LIST(TYPE, NAME, ARRAY);          \
     } else {                                                    \
-        GEN_JSON_FIELD_STRING(NAME);                            \
+        GEN_STRUCT_JSON_FIELD_STRING(NAME);                     \
     }                                                           \
     buf[len++] = ',';                                           \
     buf[len++] = ' ';
 
-#define GEN_TO_JSON(STRUCT)                                              \
+#define GEN_STRUCT_TO_JSON(STRUCT)                                       \
     static inline int STRUCT##_to_json(const STRUCT* struc, char* buf) { \
         int len = 0;                                                     \
         buf[len++] = '{';                                                \
-        STRUCT_##STRUCT(GEN_JSON_FIELD);                                 \
+        STRUCT_##STRUCT(GEN_STRUCT_JSON_FIELD);                          \
         buf[len - 2] = '}';                                              \
         buf[--len] = '\0';                                               \
         return len;                                                      \
     }
 
-#define GEN_PRIMITIVE(TYPE, FORMAT)                                         \
+#define GEN_STRUCT_PRIMITIVE(TYPE, FORMAT)                                  \
     static inline int TYPE##_serialize(const TYPE* struc, uint8_t* buf) {   \
         SERIALIZE_MEMCPY(buf, struc, sizeof(TYPE));                         \
         return sizeof(TYPE);                                                \
@@ -106,14 +106,14 @@ static inline void* reverse_memcpy(void* dst, const void* src, size_t n) {
     return dst;
 }
 
-GEN_PRIMITIVE(char, (*struc) ? "\"%c\"" : "null")
-GEN_PRIMITIVE(uint8_t, "%u")
-GEN_PRIMITIVE(uint16_t, "%u")
-GEN_PRIMITIVE(uint32_t, "%u")
-GEN_PRIMITIVE(uint64_t, "%lu")
-GEN_PRIMITIVE(int8_t, "%d")
-GEN_PRIMITIVE(int16_t, "%d")
-GEN_PRIMITIVE(int32_t, "%d")
-GEN_PRIMITIVE(int64_t, "%ld")
-GEN_PRIMITIVE(float, "%g")
-GEN_PRIMITIVE(double, "%g")
+GEN_STRUCT_PRIMITIVE(char, (*struc) ? "\"%c\"" : "null")
+GEN_STRUCT_PRIMITIVE(uint8_t, "%u")
+GEN_STRUCT_PRIMITIVE(uint16_t, "%u")
+GEN_STRUCT_PRIMITIVE(uint32_t, "%u")
+GEN_STRUCT_PRIMITIVE(uint64_t, "%lu")
+GEN_STRUCT_PRIMITIVE(int8_t, "%d")
+GEN_STRUCT_PRIMITIVE(int16_t, "%d")
+GEN_STRUCT_PRIMITIVE(int32_t, "%d")
+GEN_STRUCT_PRIMITIVE(int64_t, "%ld")
+GEN_STRUCT_PRIMITIVE(float, "%g")
+GEN_STRUCT_PRIMITIVE(double, "%g")
